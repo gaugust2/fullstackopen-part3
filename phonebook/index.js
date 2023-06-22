0027118ef1ec363c9ffa8d0a571962e9c545e572
gaugust2 <gaugust2@uwo.ca>
2023-06-22 19:53:23 -0400

    Exercise 3.16

--- phonebook/index.js
+++ phonebook/index.js
@@ -7,8 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(express.static('build'))
 
-morgan.token("content", (req) => { 
-    return req.method === "POST" ? JSON.stringify(req.body) : "" 
+morgan.token("content", (req) => {
+    return req.method === "POST" ? JSON.stringify(req.body) : ""
 })
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
@@ -44,7 +44,7 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
-      })
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -53,31 +53,28 @@ app.get('/info', (request, response) => {
     response.send(content)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-
-    /*const nameExists = persons.find(person => person.name === name)
-
-    if(nameExists){
-        return response.status(400).json({
-            error: 'name already exists in database'
-        })
-    }
-
-    const id = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER)*/
 
     const person = new Person({
         name: body.name,
@@ -88,6 +85,19 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
