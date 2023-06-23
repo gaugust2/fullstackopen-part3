94bc719fe903638d88b82bda18220364d549ad5c
gaugust2 <gaugust2@uwo.ca>
2023-06-22 23:57:55 -0400

    Exercise 3.19

--- phonebook/index.js
+++ phonebook/index.js
@@ -49,7 +49,7 @@ app.delete('/api/persons/:id', (request, response) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     const person = new Person({
@@ -60,17 +60,22 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const {name, number} = request.body
 
     const person = {
         name: body.name,
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        request.params.id,
+         {name, number}, 
+         { new: true, runValidators: true, context: 'query' }
+         )
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
